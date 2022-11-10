64a4887e91f5e39f497d7239ceaeb2c0b05212c0
Kirill Grigoryev <kirillisomska2@gmail.com>
2022-11-10 13:04:57 +0600

    [fix] fix menu

--- first_practice/scripts/script.js
+++ first_practice/scripts/script.js
@@ -1,8 +1,7 @@
 let menuBtn = document.querySelector(".header__navigation-btn");
 let navigation = document.querySelector(".header__navigation");
 
-
-menuBtn.addEventListener('click', () => {
-    menuBtn.classList.toggle('active');
-    navigation.classList.toggle('active');
-})
+menuBtn.addEventListener("click", () => {
+  menuBtn.classList.toggle("active");
+  navigation.classList.toggle("active");
+});
